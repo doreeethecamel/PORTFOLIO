48cb174595f6a5fd97eb7a48b32890edda8bc5a5
Dorelies Siloma <silomadorelies1@gmail.com>
2023-12-12 10:59:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
 
 //typed js
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Student', 'Disappointment'],
+    strings: ['Fullstack Developer', 'Student', 'Web Designer'],
     typeSpeed: 100,
     backSpeed:100,
     backDelay: 1000,
